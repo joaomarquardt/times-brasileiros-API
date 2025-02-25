cb0c70bfae83bc6881a9fbf5a2d0c6b7b3f94eae
joaomarquardt <marquardt1402@gmail.com>
2025-02-25 16:42:59 -0300

    feat: implement controllers with endpoints and http verbs methods for each domain class

---
+++ src/main/java/com/web/spring_clubs/controllers/ClubController.java
@@ -0,0 +1,49 @@
+package com.web.spring_clubs.controllers;
+
+import com.web.spring_clubs.domain.Club;
+import com.web.spring_clubs.services.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clubs")
+public class ClubController {
+
+    @Autowired
+    private ClubService service;
+
+    @GetMapping
+    public ResponseEntity<List<Club>> findAll() {
+        List<Club> entities = service.findAll();
+        return new ResponseEntity<>(entities,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "{/id}")
+    public ResponseEntity<?> findClubById(@PathVariable(value = "id") Long id) {
+        Club entity = service.findClubById(id);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createClub(@RequestBody Club club) {
+        Club entity = service.createClub(club);
+        return new ResponseEntity<>(entity, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> updateClub(@PathVariable(value = "id") Long id, @RequestBody Club club) {
+        Club entity = service.updateClub(id, club);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteClub(@PathVariable(value = "id") Long id) {
+        service.deleteClub(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/main/java/com/web/spring_clubs/controllers/CoachController.java
@@ -0,0 +1,49 @@
+package com.web.spring_clubs.controllers;
+
+import com.web.spring_clubs.domain.Coach;
+import com.web.spring_clubs.services.CoachService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/coaches")
+public class CoachController {
+
+    @Autowired
+    private CoachService service;
+
+    @GetMapping
+    public ResponseEntity<List<Coach>> findAll() {
+        List<Coach> entities = service.findAll();
+        return new ResponseEntity<>(entities,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "{/id}")
+    public ResponseEntity<?> findCoachById(@PathVariable(value = "id") Long id) {
+        Coach entity = service.findCoachById(id);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createCoach(@RequestBody Coach coach) {
+        Coach entity = service.createCoach(coach);
+        return new ResponseEntity<>(entity, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> updateCoach(@PathVariable(value = "id") Long id, @RequestBody Coach coach) {
+        Coach entity = service.updateCoach(id, coach);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteCoach(@PathVariable(value = "id") Long id) {
+        service.deleteCoach(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
