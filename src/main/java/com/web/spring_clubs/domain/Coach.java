9c260968810f78e1539a3f7b107acaf2ee15bb24
joaomarquardt <marquardt1402@gmail.com>
2025-02-25 18:41:10 -0300

    feat: change id attribute from specifics classes to employee class

--- src/main/java/com/web/spring_clubs/domain/Coach.java
+++ src/main/java/com/web/spring_clubs/domain/Coach.java
@@ -7,20 +7,8 @@ import jakarta.validation.constraints.PositiveOrZero;
 @Table(name = "coaches")
 @Entity
 public class Coach extends Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @PositiveOrZero
     private int yearsOfExperience;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public int getYearsOfExperience() {
         return yearsOfExperience;
--- src/main/java/com/web/spring_clubs/domain/Player.java
+++ src/main/java/com/web/spring_clubs/domain/Player.java
@@ -9,23 +9,11 @@ import java.util.List;
 @Table(name = "players")
 @Entity
 public class Player extends Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @NotEmpty
     @ElementCollection
     @CollectionTable(name = "player_positions", joinColumns = @JoinColumn(name = "player_id"))
     @Column(name = "position")
     private List<String> positions;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public List<String> getPositions() {
         return positions;
