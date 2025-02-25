d6dd15dbaf5d8113ad216610f1a0ab00a7c29ec7
joaomarquardt <marquardt1402@gmail.com>
2025-02-25 14:49:50 -0300

    first commit

---
+++ src/main/java/com/web/spring_clubs/BrazilianClubsApiApplication.java
@@ -0,0 +1,13 @@
+package com.web.spring_clubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilianClubsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilianClubsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/spring_clubs/BrazilianClubsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.spring_clubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilianClubsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
