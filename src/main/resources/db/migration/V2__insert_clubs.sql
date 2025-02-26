6aa6d1981fce5f7bae6abe143d2d39d71e439451
joaomarquardt <marquardt1402@gmail.com>
2025-02-26 12:45:24 -0300

    feat: insert top Brazilian league clubs for flyway migration

---
+++ src/main/resources/db/migration/V2__insert_clubs.sql
@@ -0,0 +1,29 @@
+-- Inserção de clubes
+INSERT INTO CLUBS (ID, NAME, SURNAME, STATE, CITY, FOUNDED_AT, STADIUM, LOGO_URL)
+VALUES
+(1, 'Atlético-MG', 'Galo', 'MG', 'Belo Horizonte', '1908-03-25', 'Mineirão', ''),
+(2, 'Bahia', 'Tricolor de Aço', 'BA', 'Salvador', '1931-01-01', 'Arena Fonte Nova', ''),
+(3, 'Botafogo', 'Alvinegro', 'RJ', 'Rio de Janeiro', '1904-07-12', 'Estádio Nilton Santos', ''),
+(4, 'Ceará', 'Vozão', 'CE', 'Fortaleza', '1914-06-02', 'Arena Castelão', ''),
+(5, 'Corinthians', 'Timão', 'SP', 'São Paulo', '1910-09-01', 'Arena Corinthians', ''),
+(6, 'Cruzeiro', 'Raposa', 'MG', 'Belo Horizonte', '1921-01-02', 'Mineirão', ''),
+(7, 'Flamengo', 'Urubu', 'RJ', 'Rio de Janeiro', '1895-11-15', 'Maracanã', ''),
+(8, 'Fluminense', 'Time de Guerreiros', 'RJ', 'Rio de Janeiro', '1902-07-21', 'Maracanã', ''),
+(9, 'Fortaleza', 'Leão do Pici', 'CE', 'Fortaleza', '1918-10-18', 'Arena Castelão', ''),
+(10, 'Grêmio', 'Imortal', 'RS', 'Porto Alegre', '1903-09-15', 'Arena do Grêmio', ''),
+(11, 'Internacional', 'Colorado', 'RS', 'Porto Alegre', '1909-04-04', 'Beira-Rio', ''),
+(12, 'Juventude', 'Papo', 'RS', 'Caxias do Sul', '1913-06-29', 'Alfredo Jaconi', ''),
+(13, 'Mirassol', 'Leão da Alta Sorocabana', 'SP', 'Mirassol', '1925-07-01', 'Estádio José Maria de Campos Maia', ''),
+(14, 'Palmeiras', 'Porco', 'SP', 'São Paulo', '1914-08-26', 'Allianz Parque', ''),
+(15, 'RB Bragantino', 'Massa Bruta', 'SP', 'Bragança Paulista', '1928-06-08', 'Arena Bragantino', ''),
+(16, 'Santos', 'Peixe', 'SP', 'Santos', '1912-04-14', 'Vila Belmiro', ''),
+(17, 'São Paulo', 'Tricolor Paulista', 'SP', 'São Paulo', '1930-08-25', 'Morumbi', ''),
+(18, 'Sport', 'Leão da Ilha', 'PE', 'Recife', '1905-05-13', 'Arena Pernambuco', ''),
+(19, 'Vasco', 'Gigante da Colina', 'RJ', 'Rio de Janeiro', '1898-08-21', 'Maracanã', ''),
+(20, 'Vitória', 'Leão da Barra', 'BA', 'Salvador', '1899-05-13', 'Barradão', '');
+
+
+
+
+
+
