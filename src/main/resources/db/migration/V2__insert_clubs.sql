2b6b3d9d971ce670ec529f744a9da6c9f27bf54c
joaomarquardt <marquardt1402@gmail.com>
2025-03-07 10:20:28 -0300

    fix: remove id column in insert intos from flyway to fix errors

--- src/main/resources/db/migration/V2__insert_clubs.sql
+++ src/main/resources/db/migration/V2__insert_clubs.sql
@@ -1,26 +1,26 @@
 -- Inserção de clubes
-INSERT INTO CLUBS (ID, NAME, SURNAME, STATE, CITY, FOUNDED_AT, STADIUM, LOGO_PATH)
+INSERT INTO CLUBS (NAME, SURNAME, STATE, CITY, FOUNDED_AT, STADIUM, LOGO_PATH)
 VALUES
-(1, 'Atlético-MG', 'Galo', 'MG', 'Belo Horizonte', '1908-03-25', 'Mineirão', 'images/clubs/atletico_mineiro.png'),
-(2, 'Bahia', 'Tricolor de Aço', 'BA', 'Salvador', '1931-01-01', 'Arena Fonte Nova', 'images/clubs/bahia.png'),
-(3, 'Botafogo', 'Alvinegro', 'RJ', 'Rio de Janeiro', '1904-07-12', 'Estádio Nilton Santos', 'images/clubs/botafogo.png'),
-(4, 'Ceará', 'Vozão', 'CE', 'Fortaleza', '1914-06-02', 'Arena Castelão', 'images/clubs/ceara.png'),
-(5, 'Corinthians', 'Timão', 'SP', 'São Paulo', '1910-09-01', 'Arena Corinthians', 'images/clubs/corinthians.png'),
-(6, 'Cruzeiro', 'Raposa', 'MG', 'Belo Horizonte', '1921-01-02', 'Mineirão', 'images/clubs/cruzeiro.png'),
-(7, 'Flamengo', 'Urubu', 'RJ', 'Rio de Janeiro', '1895-11-15', 'Maracanã', 'images/clubs/flamengo.png'),
-(8, 'Fluminense', 'Time de Guerreiros', 'RJ', 'Rio de Janeiro', '1902-07-21', 'Maracanã', 'images/clubs/fluminense.png'),
-(9, 'Fortaleza', 'Leão do Pici', 'CE', 'Fortaleza', '1918-10-18', 'Arena Castelão', 'images/clubs/fortaleza.png'),
-(10, 'Grêmio', 'Imortal', 'RS', 'Porto Alegre', '1903-09-15', 'Arena do Grêmio', 'images/clubs/gremio.png'),
-(11, 'Internacional', 'Colorado', 'RS', 'Porto Alegre', '1909-04-04', 'Beira-Rio', 'images/clubs/internacional.png'),
-(12, 'Juventude', 'Papo', 'RS', 'Caxias do Sul', '1913-06-29', 'Alfredo Jaconi', 'images/clubs/juventude.png'),
-(13, 'Mirassol', 'Leão da Alta Sorocabana', 'SP', 'Mirassol', '1925-07-01', 'Estádio José Maria de Campos Maia', 'images/clubs/mirassol.png'),
-(14, 'Palmeiras', 'Porco', 'SP', 'São Paulo', '1914-08-26', 'Allianz Parque', 'images/clubs/palmeiras.png'),
-(15, 'RB Bragantino', 'Massa Bruta', 'SP', 'Bragança Paulista', '1928-06-08', 'Arena Bragantino', 'images/clubs/red_bull_bragantino.png'),
-(16, 'Santos', 'Peixe', 'SP', 'Santos', '1912-04-14', 'Vila Belmiro', 'images/clubs/santos.png'),
-(17, 'São Paulo', 'Tricolor Paulista', 'SP', 'São Paulo', '1930-08-25', 'Morumbi', 'images/clubs/sao_paulo.png'),
-(18, 'Sport', 'Leão da Ilha', 'PE', 'Recife', '1905-05-13', 'Arena Pernambuco', 'images/clubs/sport.png'),
-(19, 'Vasco', 'Gigante da Colina', 'RJ', 'Rio de Janeiro', '1898-08-21', 'Maracanã', 'images/clubs/vasco_da_gama.png'),
-(20, 'Vitória', 'Leão da Barra', 'BA', 'Salvador', '1899-05-13', 'Barradão', 'images/clubs/vitoria.png');
+('Atlético-MG', 'Galo', 'MG', 'Belo Horizonte', '1908-03-25', 'Mineirão', 'images/clubs/atletico_mineiro.png'),
+('Bahia', 'Tricolor de Aço', 'BA', 'Salvador', '1931-01-01', 'Arena Fonte Nova', 'images/clubs/bahia.png'),
+('Botafogo', 'Alvinegro', 'RJ', 'Rio de Janeiro', '1904-07-12', 'Estádio Nilton Santos', 'images/clubs/botafogo.png'),
+('Ceará', 'Vozão', 'CE', 'Fortaleza', '1914-06-02', 'Arena Castelão', 'images/clubs/ceara.png'),
+('Corinthians', 'Timão', 'SP', 'São Paulo', '1910-09-01', 'Arena Corinthians', 'images/clubs/corinthians.png'),
+('Cruzeiro', 'Raposa', 'MG', 'Belo Horizonte', '1921-01-02', 'Mineirão', 'images/clubs/cruzeiro.png'),
+('Flamengo', 'Urubu', 'RJ', 'Rio de Janeiro', '1895-11-15', 'Maracanã', 'images/clubs/flamengo.png'),
+('Fluminense', 'Time de Guerreiros', 'RJ', 'Rio de Janeiro', '1902-07-21', 'Maracanã', 'images/clubs/fluminense.png'),
+('Fortaleza', 'Leão do Pici', 'CE', 'Fortaleza', '1918-10-18', 'Arena Castelão', 'images/clubs/fortaleza.png'),
+('Grêmio', 'Imortal', 'RS', 'Porto Alegre', '1903-09-15', 'Arena do Grêmio', 'images/clubs/gremio.png'),
+('Internacional', 'Colorado', 'RS', 'Porto Alegre', '1909-04-04', 'Beira-Rio', 'images/clubs/internacional.png'),
+('Juventude', 'Papo', 'RS', 'Caxias do Sul', '1913-06-29', 'Alfredo Jaconi', 'images/clubs/juventude.png'),
+('Mirassol', 'Leão da Alta Sorocabana', 'SP', 'Mirassol', '1925-07-01', 'Estádio José Maria de Campos Maia', 'images/clubs/mirassol.png'),
+('Palmeiras', 'Porco', 'SP', 'São Paulo', '1914-08-26', 'Allianz Parque', 'images/clubs/palmeiras.png'),
+('RB Bragantino', 'Massa Bruta', 'SP', 'Bragança Paulista', '1928-06-08', 'Arena Bragantino', 'images/clubs/red_bull_bragantino.png'),
+('Santos', 'Peixe', 'SP', 'Santos', '1912-04-14', 'Vila Belmiro', 'images/clubs/santos.png'),
+('São Paulo', 'Tricolor Paulista', 'SP', 'São Paulo', '1930-08-25', 'Morumbi', 'images/clubs/sao_paulo.png'),
+('Sport', 'Leão da Ilha', 'PE', 'Recife', '1905-05-13', 'Arena Pernambuco', 'images/clubs/sport.png'),
+('Vasco', 'Gigante da Colina', 'RJ', 'Rio de Janeiro', '1898-08-21', 'Maracanã', 'images/clubs/vasco_da_gama.png'),
+('Vitória', 'Leão da Barra', 'BA', 'Salvador', '1899-05-13', 'Barradão', 'images/clubs/vitoria.png');
 
 
 
