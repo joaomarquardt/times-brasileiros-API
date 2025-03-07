77452125244b594bf1588dc4e404ce96f5c10a20
joaomarquardt <marquardt1402@gmail.com>
2025-03-07 17:25:34 -0300

    fix: remove generic mapper and implement generic methods inside specific mappers

--- src/main/java/com/web/spring_clubs/mappers/ClubMapper.java
+++ src/main/java/com/web/spring_clubs/mappers/ClubMapper.java
@@ -3,7 +3,24 @@ package com.web.spring_clubs.mappers;
 import com.web.spring_clubs.domain.Club;
 import com.web.spring_clubs.dtos.ClubDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface ClubMapper extends GenericMapper<Club, ClubDTO> {
+public interface ClubMapper {
+    ClubMapper INSTANCE = Mappers.getMapper(ClubMapper.class);
+
+    ClubDTO toDTO(Club entity);
+
+    Club toEntity(ClubDTO dto);
+
+    List<ClubDTO> toDTOList(List<Club> entities);
+
+    List<Club> toEntityList(List<ClubDTO> dtos);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDTO(ClubDTO dto, @MappingTarget Club entity);
 }
--- src/main/java/com/web/spring_clubs/mappers/CoachMapper.java
+++ src/main/java/com/web/spring_clubs/mappers/CoachMapper.java
@@ -3,7 +3,24 @@ package com.web.spring_clubs.mappers;
 import com.web.spring_clubs.domain.Coach;
 import com.web.spring_clubs.dtos.CoachDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface CoachMapper extends GenericMapper<Coach, CoachDTO> {
+public interface CoachMapper {
+    CoachMapper INSTANCE = Mappers.getMapper(CoachMapper.class);
+
+    CoachDTO toDTO(Coach entity);
+
+    Coach toEntity(CoachDTO dto);
+
+    List<CoachDTO> toDTOList(List<Coach> entities);
+
+    List<Coach> toEntityList(List<CoachDTO> dtos);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDTO(CoachDTO dto, @MappingTarget Coach entity);
 }
