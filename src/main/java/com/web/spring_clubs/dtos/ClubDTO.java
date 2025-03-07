5c76dd92e0c26bf3583af015dbdec25bedd2552e
joaomarquardt <marquardt1402@gmail.com>
2025-03-07 17:27:08 -0300

    feat: add @Past annotation in date attribute

--- src/main/java/com/web/spring_clubs/dtos/ClubDTO.java
+++ src/main/java/com/web/spring_clubs/dtos/ClubDTO.java
@@ -2,6 +2,7 @@ package com.web.spring_clubs.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 import java.time.LocalDate;
 
@@ -14,6 +15,7 @@ public record ClubDTO(
         @NotBlank
         String city,
         @NotNull
+        @Past
         LocalDate foundedAt,
         @NotBlank
         String stadium,
