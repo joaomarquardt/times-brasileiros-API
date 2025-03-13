b3ff5b76f296c6d8ad884356328b72bf45e510c4
joaomarquardt <marquardt1402@gmail.com>
2025-03-13 16:57:58 -0300

    feat: add authentication service with login and registration logic

---
+++ src/main/java/com/web/spring_clubs/security/services/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.web.spring_clubs.security.services;
+
+import com.web.spring_clubs.security.domain.User;
+import com.web.spring_clubs.security.dtos.AuthDTO;
+import com.web.spring_clubs.security.dtos.RegisterDTO;
+import com.web.spring_clubs.security.exceptions.RegisterConflictException;
+import com.web.spring_clubs.security.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class AuthenticationService {
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void login(AuthDTO authDTO) {
+        repository.findByUsername(authDTO.username()).orElseThrow(() -> new UsernameNotFoundException("User not found by username"));
+        var usernamePassword = new UsernamePasswordAuthenticationToken(authDTO.username(), authDTO.password());
+        try {
+            this.authenticationManager.authenticate(usernamePassword);
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+    }
+
+    public void register(RegisterDTO registerDTO) {
+        if (repository.findByUsername(registerDTO.username()).isPresent()) {
+            throw new RegisterConflictException("Username is already in use");
+        }
+        if (repository.findByEmail(registerDTO.email()).isPresent()) {
+            throw new RegisterConflictException("Email already in use");
+        }
+        String encryptedPassword = passwordEncoder.encode(registerDTO.password());
+        User newUser = new User(registerDTO.username(), registerDTO.email(), encryptedPassword, registerDTO.role());
+        repository.save(newUser);
+    }
+}
