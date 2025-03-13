b03cfe05d9b15394bf7d9ce0bf3c1c1afd9cc336
joaomarquardt <marquardt1402@gmail.com>
2025-03-13 16:59:29 -0300

    feat: add user repository

---
+++ src/main/java/com/web/spring_clubs/security/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.web.spring_clubs.security.repositories;
+
+import com.web.spring_clubs.security.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
