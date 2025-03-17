41bc2349c7a940e7bf74821604b4cbae52c1103b
joaomarquardt <marquardt1402@gmail.com>
2025-03-17 14:09:39 -0300

    feat: add personalized UnauthorizedAccessException and handle with it in GlobalExceptionHandler

--- src/main/java/com/web/spring_clubs/infra/GlobalExceptionHandler.java
+++ src/main/java/com/web/spring_clubs/infra/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.web.spring_clubs.infra;
 
+import com.web.spring_clubs.security.exceptions.UnauthorizedAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -20,6 +19,12 @@ public class GlobalExceptionHandler {
         e.getBindingResult().getFieldErrors()
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         RestErrorResponse errorResponse = new RestErrorResponse(HttpStatus.BAD_REQUEST.value(), errors);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(UnauthorizedAccessException.class)
+    public ResponseEntity<RestErrorResponse> handleUnauthorizedAccessError(UnauthorizedAccessException e) {
+        RestErrorResponse errorResponse = new RestErrorResponse(HttpStatus.FORBIDDEN.value(), e.getMessage());
         return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 }
--- src/main/java/com/web/spring_clubs/infra/RestErrorResponse.java
+++ src/main/java/com/web/spring_clubs/infra/RestErrorResponse.java
@@ -1,15 +1,26 @@
 package com.web.spring_clubs.infra;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import java.util.Map;
 
 public class RestErrorResponse {
     private int status;
-    private final Map<String, String> errors;
+    @JsonInclude(Include.NON_NULL)
+    private String error;
+    @JsonInclude(Include.NON_NULL)
+    private Map<String, String> errors;
 
     public RestErrorResponse(int status, Map<String, String> errors) {
         this.status = status;
         this.errors = errors;
+    }
+
+    public RestErrorResponse(int status, String error) {
+        this.status = status;
+        this.error = error;
     }
 
     public void setStatus(int status) {
@@ -22,5 +33,9 @@ public class RestErrorResponse {
 
     public Map<String, String> getErrors() {
         return errors;
+    }
+
+    public String getError() {
+        return error;
     }
 }
